185317026bbc56584e8204eb5b612d3c7f1531d2
Randy Neu <rneu31@gmail.com>
2017-10-01 17:26:07 -0500

    Split on positions (more robust) and format in cols

--- parse_dk_results.py
+++ parse_dk_results.py
@@ -2,12 +2,7 @@
 # This script takes input of a Draft Kings contest standings zip file
 # and outputs the lineups formatted in position order into output.csv.
 #
-# Step 1) Change the file_name variable below to the file name
-# of the contest standings zip file you have downloaded.
-# 
-# Step 2) run this script
-# 
-# Step 3) HAVE FUN!
+# See the README for usage information.
 #
 # WINsconsinSports
 #
@@ -15,19 +10,31 @@ import pandas as pd
 import re
 # csv of form:
 # Rank,EntryId,EntryName,TimeRemaining,Points,Lineup
-file_name="contest-standings-45286830.zip"
-player_lineups=pd.read_csv(file_name)
+file_name="contest-standings.zip"
+parsed_results=pd.read_csv(file_name)
 POSITIONS=["QB","RB","WR","TE","FLEX","DST"]
-lineup_list=[]
-for idx, lineup in enumerate(player_lineups['Lineup'].dropna()):
+
+
+lineup_list = []
+for lineup in parsed_results['Lineup'].dropna():
     # Each lineup is listed in the "Lineup" column in the form:
     # POSITION FirstName LastName POSITION FirstNameLastName ...
-    regex = "([A-Z][A-Z]+)\s([\w'.]*[\s][\w]*)"
-    parsed_lineup = re.findall(regex, lineup)
-    lineup_list.append(parsed_lineup)
 
-csv_output = 'position,player\n'
-for lineup in lineup_list:
+    # Split on valid positions (keeping positions in the result)
+    lineup_components = re.split('(QB|RB|WR|TE|FLEX|DST)', lineup)
+
+    # Remove first element if it is empty (it will be)
+    if not lineup_components[0]:
+        lineup_components = lineup_components[1:]
+
+    # Since the later logic relies on a list of tuples with (position, player),
+    # force our data into that form (lazy).
+    lineup_list.append(
+        [tuple(lineup_components[i:i+2]) for i in range(0, len(lineup_components), 2)]
+    )
+
+csv_output = 'user,points,qb,rb1,rb2,wr1,wr2,wr3,te,flex,dst\n'
+for idx, lineup in enumerate(lineup_list):
     formatted_lineup = {}
     # The lineup is now a list of tuples containing
     # position and player in the form of:
@@ -41,15 +48,14 @@ for lineup in lineup_list:
         else:
             # position not added to lineup yet, add it (list form).
             formatted_lineup[filled_position[0]] = [filled_position[1]]
-    
+
+    csv_output += f"{parsed_results['EntryName'][idx]},{parsed_results['Points'][idx]},"
     # Loop over POSITIONS specified above and add the
     # position,player to the csv storage string (written as file later).
     for pos in POSITIONS:
         for player_at_pos in formatted_lineup[pos]:
-            csv_output += f"{pos},{player_at_pos}"
-            csv_output += '\n'
+            csv_output += f"{player_at_pos.strip(' ')},"
     csv_output += '\n'
 
 with open('output.csv', 'w') as out_file:
     out_file.write(csv_output)
-  
